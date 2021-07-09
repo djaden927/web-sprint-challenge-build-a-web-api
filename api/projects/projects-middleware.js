5502fea3785fbc3004da54813e361d376981b88c
djaden927 <djaden92@gmail.com>
2021-07-09 15:56:12 -0600

    done

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,63 @@
 // add middlewares here related to projects
+// const projectRouter = require('./projects-router')
+// const actionRouter = require('../actions/actions-router');
+const Projects = require('./projects-model');
+
+const validateCompleted = (req, res, next) => {
+    if(!req.body.hasOwnProperty('completed')){ //eslint-disable-line
+        next({
+            status: 400
+        })
+    } else{
+        next()
+    }
+}
+
+const validateProjectId = async (req, res, next) => {
+    try{
+        const {id} = req.params;
+        const project = await Projects.get(id)
+        if(project){
+            req.project = project
+            next()
+        } else{
+            next({
+                status: 404,
+                message: "validate project id"
+            })
+        }
+    }catch (err){
+        next(err)
+    }
+}
+
+const validateProjectBody = (req, res, next) => {
+    if(!req.body.name || !req.body.description){
+        next({
+            status: 400
+        })
+    } else{
+        next()
+    }
+}
+
+const notFound = (req, res) => {
+    res.status(404).json({
+      message: 'not found, sorry!'
+    })
+  }
+  
+  const errorHandling = (err, req, res, next) => { // eslint-disable-line
+    const status = err.status || 500
+    res.status(status).json({
+      message: err.message,
+    })
+  }
+
+  module.exports = {
+    notFound,
+    errorHandling,
+    validateProjectId,
+    validateProjectBody,
+    validateCompleted
+  }
--- api/server.js
+++ api/server.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const projectRouter = require('./projects/projects-router')
+const actionRouter = require('./actions/actions-router');
+
+const {
+    notFound,
+    errorHandling
+} = require('./projects/projects-middleware')
+
+
+
+
 const server = express();
+server.use(express.json())
+
+
+server.use('/api/projects', projectRouter)
+server.use('/api/actions', actionRouter)
+
+server.use('*', notFound)
+
+server.use(errorHandling)
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
